de4ae6d5d80eeda5ca699c442db2af60c27bbcc4
Albin Groen <albin.groen@gmail.com>
2022-05-09 13:34:11 +0200

    Initial Commit

---
+++ coiner-mac/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  coiner-mac
+//
+//  Created by Albin Groen on 2022-05-09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ coiner-mac/coiner_macApp.swift
@@ -0,0 +1,17 @@
+//
+//  coiner_macApp.swift
+//  coiner-mac
+//
+//  Created by Albin Groen on 2022-05-09.
+//
+
+import SwiftUI
+
+@main
+struct coiner_macApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
